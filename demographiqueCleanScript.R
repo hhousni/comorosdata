77cb171e26af51ca4f69a6656c7be2c33545d8c8
hhousni <happrend2@gmail.com>
2023-03-04 17:41:47 +1300

    integrated 2023 population

--- demographiqueCleanScript.R
+++ demographiqueCleanScript.R
@@ -6,82 +6,100 @@
 library(pacman)
 p_load(tidyverse, janitor, readxl, tmap, nycflights13,leaflet, sp, sf,leaflet.extras)
 
-# 1.0 Data input ----
-demo_data <- read_excel("Projection démographiques 2017-2042 Inseed Comores.xlsx",
-                        sheet = "Structure 2018-2042")
+# # Read in the data
+# demo_data <- read_excel("Projection démographiques 2017-2042 Inseed Comores.xlsx", sheet = "Structure 2018-2042")
+#
+# # Filter the data
+# filtered_data <- demo_data %>%
+#   filter(`Population by age and sex  - (Total) (Male+Female)` %in% c("Decoupage", "Total"))
+#
+# # Use meaningful variable names
+# filtered_data <- select(filtered_data, 2:13)
+#
+# # Convert columns to character
+# demo_data_v2 <- as.data.frame(lapply(filtered_data, as.character))
+#
+# # Add new row
+# new_row <- c("Total_National", "Male_National", "Female_National", "Total_Mohéli", "Male_Mohéli", "Female_Mohéli",
+#              "Total_Anjouan", "Male_Anjouan", "Female_Anjouan", "Total_Grande Comore", "Male_Grande Comore", "Female_Grande Comore")
+#
+#
+# demo_data_v3 <- rbind(demo_data_v2, new_row)
+#
+# # Subset demo_data_v3 and assign column names
+# demo_data_v4 <- setNames(demo_data_v3[1:25, ], demo_data_v3[26, ])
+#
+# # Convert to long format
+# demo_data_v5 <- pivot_longer(demo_data_v4, cols = c(1:12),
+#                              names_to = "variable",
+#                              values_to = "value")
+#
+# demo_datav6 <- demo_data_v5 %>%
+#   mutate(annee = rep(c(2018:2042), each = 12)) %>%
+#   separate(variable, c("sex", "decoupage"), sep = "_") %>%
+#   select(4,2,1,3) %>%
+#   mutate(value = as.numeric(value))
+#
+#
+# kmDemoData <- demo_datav6
+#
+# save(kmDemoData, file = "kmDemoData.Rdata")
 
-# 1.1 Data processing ----
-demo_data$`Population by age and sex  - (Total) (Male+Female)`[2] <-  "Decoupage"
+load("kmDemoData.Rdata")
 
-demo_datav1 <-  demo_data %>%
-  filter(`Population by age and sex  - (Total) (Male+Female)`  %in% c("Decoupage","Total"))
+# select data for 2023
 
-# names first as header
-names(demo_datav1) <- demo_datav1 [1,]
-
-demo_datav1 <- demo_datav1[,2:13]
-
-demo_datav1 [] <- lapply (demo_datav1, as.character)
-
-new_row <- c("Total_National","Male_National","Female_National","Total_Mwali","Male_Mwali","Female_Mwali",
-                                         "Total_Ndzuwani","Male_Ndzuwani","Female_Ndzuwani","Total_Ngazidja","Male_Ngazidja",
-                                         "Female_Ngazidja")
-
-demo_datav2 <- rbind(demo_datav1,new_row)
-
-demo_datav3 <- demo_datav2[c(27,2:26),]
-
-names(demo_datav3) <- demo_datav3[1,]
-demo_datav3 <- demo_datav3[2:26,]
+kmData2023 <- kmDemoData %>%
+  filter(annee == 2023)
 
 
-demo_datav4 <- pivot_longer(demo_datav3, cols = c(1:12),
-                            names_to = "variable",
-                            values_to = "value")
+# usage of comoros maps
+library(comorosmaps)
 
+map <- comoros("island")
 
-demo_datav5 <- demo_datav4 %>%
-  mutate(annee = rep(c(2018:2042), each = 12)) %>%
-  separate(variable, c("sex","decoupage")) %>%
-  select(4,2,1,3) %>%
-  mutate(value = as.numeric(value))
+population_stDB <- map %>%
+  left_join(kmData2023, by = c("name" = "decoupage")) %>%
+  pivot_wider(id_cols = c("name", "annee", "geometry"),
+              names_from = "sex",
+              values_from = "value")
 
-population <- demo_datav5 %>% filter(decoupage %in% c("Mwali","Ndzuwani","Ngazidja"),
-                                sex == "Total",
-                                annee == 2023) %>%
-  mutate (decoupage = case_when(
-    decoupage == "Mwali" ~ "Mohéli",
-    decoupage == "Ndzuwani" ~ "Anjouan",
-    decoupage == "Ngazidja" ~ "Grande Comore"
-  ))
-
-st_db <- comoros("island")
-
-population_stDB <- st_db %>% left_join(population, by = c("name"="decoupage"))
 
 tmap_mode("view") # in order to set tmap interactive
-try <- tm_shape(population_stDB) +
-  tm_polygons(col = "value")
+try <- tm_shape(population_stDB ) +
+  tm_fill()
 
 
 
-cities <- data.frame(
-  city = c("Moroni", "Moutsamoudou", "Fomboni"),
-  lng = c(43.25506, 44.39944, 43.7425),
-  lat = c(-11.70216, -12.16672, -12.28),
-  pop = c(1,5,6),
-  this = c("b","c","d")
 
-) %>%
-  mutate(lng = as.numeric(lng),
-         lat = as.numeric(lat))
+# using Leaflet
+# poly_centroid <- st_centroid(population_stDB) %>%
+#   mutate(lng = st_coordinates(.)[1],
+#          lat = st_coordinates(.)[2]) %>%
+#   mutate(value_formatted = format(value, big.mark = " "))
+#
+# try_leaflet <- tmap_leaflet(try) %>%
+#   addMarkers(data = poly_centroid,
+#              label = paste(poly_centroid$name, "<br>",
+#                            poly_centroid$value_formatted, "habitants") %>%
+#                lapply(htmltools::HTML))
 
-try_leflet <- tmap_leaflet(try) %>%
-  addMarkers(data = cities,
-             label = paste(cities$city, "<br>",
-                           cities$pop, "habitants", "<br>",
-                           "essaye: ", cities$this) %>%
-  lapply(htmltools::HTML))
 
-try_leflet
+
+library(tidyr)
+
+# create a sample data frame
+df <- data.frame(id = c(1, 2, 3),
+                 category = c("A", "B", "C"),
+                 value = c(10, 20, 30))
+
+# reshape the data frame from long to wide for the "category" column
+df_wide <- pivot_wider(df, id_cols = id, names_from = category, values_from = value)
+
+
+
+
+
+
+
 
